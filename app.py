99f1f70649ffa3124bf16e96bea49e2330cfea54
Cleyton Muto <cleytonmuto@gmail.com>
2024-04-03 09:32:57 -0300

    init: first commit

---
+++ app.py
@@ -0,0 +1,24 @@
+def main():
+    wget_output = ["#/bin/bash"]
+    ocr_output = ["#/bin/bash"]
+    with open("1952.html", "rt", encoding="UTF-8") as file:
+        lines = file.readlines()
+        for line in lines:
+            line = line[:-1]
+            if line.find("pdf") > 0:
+                tokens = line.split("\"")
+                for token in tokens:
+                    if token.find("arquivos") > 0:
+                        pieces = token.split("/")
+                        wget_output.append("wget https://www.ioepa.com.br" + token)
+                        ocr_output.append("ocrmypdf input/" + pieces[3] + " output/" + pieces[3])
+    with open("input/script.sh", "wt", encoding="UTF-8") as file:
+        for line in wget_output:
+            file.write(line + "\n")
+    with open("ocr_script.sh", "wt", encoding="UTF-8") as file:
+        for line in ocr_output:
+            file.write(line + "\n")
+    
+    
+if __name__ == "__main__":
+    main()
